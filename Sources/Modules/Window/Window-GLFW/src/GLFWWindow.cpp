133e833d2d1b18a47cc8c9e394a735e04be9155c
CallMeTerry-ZED <callmeterryv2@gmail.com>
2025-11-06 05:05:06 -0500

    GLFW Window Module Now Uses Our Event System

--- Sources/Modules/Window/Window-GLFW/src/GLFWWindow.cpp
+++ Sources/Modules/Window/Window-GLFW/src/GLFWWindow.cpp
@@ -3,6 +3,7 @@
  */
 
 #include "Window-GLFW/GLFWWindow.h"
+#include "Engine/Events/EventSystem.h"
 #include <iostream>
 
 namespace ZED
@@ -23,19 +24,59 @@ namespace ZED
             return false;
         }
 
+        // Make this window’s context current
         glfwMakeContextCurrent(m_Window);
+
+        // Store this pointer for use in callbacks
+        glfwSetWindowUserPointer(m_Window, this);
+
+        // Window close callback
+        glfwSetWindowCloseCallback(m_Window, [](GLFWwindow* window)
+        {
+            auto* self = static_cast<GLFWWindow*>(glfwGetWindowUserPointer(window));
+            if (!self) return;
+            // Mark it no longer running
+            self->m_running = false;
+            // Post a WindowClose event
+            ZED::Event ev{};
+            ev.type = ZED::EventType::WindowClose;
+            ZED::EventSystem::Get().Post(ev);
+        });
+
+        // Window resize callback
+        glfwSetWindowSizeCallback(m_Window, [](GLFWwindow*, int w, int h)
+        {
+            ZED::Event ev{};
+            ev.type = ZED::EventType::WindowResized;
+            ev.a = w;
+            ev.b = h;
+            ZED::EventSystem::Get().Post(ev);
+        });
+
+        // Window focus callback (0 = lost, 1 = gained)
+        glfwSetWindowFocusCallback(m_Window, [](GLFWwindow*, int focused)
+        {
+            ZED::Event ev{};
+            ev.type = focused ? ZED::EventType::WindowFocusGained
+                              : ZED::EventType::WindowFocusLost;
+            ZED::EventSystem::Get().Post(ev);
+        });
 
         return true;
     }
 
     void GLFWWindow::PollEvents()
     {
+        // Process all queued OS events.  Callbacks will publish window events.
         glfwPollEvents();
 
+        // If an external call flagged this window for closing, publish a close event.
         if (glfwWindowShouldClose(m_Window))
         {
             m_running = false;
-            Shutdown();
+            ZED::Event ev{};
+            ev.type = ZED::EventType::WindowClose;
+            ZED::EventSystem::Get().Post(ev);
         }
     }
 
